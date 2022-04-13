3ca678f099cce46a93685dc9cac31d5da67e9827
elijahrosett <rosett3@gmail.com>
2022-04-13 15:09:11 -0600

    Registered car model with admin site + completed post function for car + updated restframework decimal settings

--- cars/views.py
+++ cars/views.py
@@ -1,9 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from .models import Car
+from .serializers import CarSerializer
+from rest_framework import status
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
+    
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
 
 
-    return Response("ok")
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- cars_project/settings.py
+++ cars_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,3 +117,7 @@ try:
     from cars_project.local_setting import *
 except ImportError:
     pass
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': FALSE
+}
